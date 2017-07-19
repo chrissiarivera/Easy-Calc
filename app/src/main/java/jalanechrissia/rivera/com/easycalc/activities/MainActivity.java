96d547a2b886e67529c9f1ecd406b3f0a072d0fe
Jalane Chrissia Rivera <riverajalane@gmail.com>
2017-07-19 19:16:00 +0800

    Implemented layout design and styling

--- app/src/main/java/jalanechrissia/rivera/com/easycalc/activities/MainActivity.java
+++ app/src/main/java/jalanechrissia/rivera/com/easycalc/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package jalanechrissia.rivera.com.easycalc;
+package jalanechrissia.rivera.com.easycalc.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import jalanechrissia.rivera.com.easycalc.R;
 
 public class MainActivity extends AppCompatActivity {
 
